9f8ccd6586a9fb4b21cb220a20693496953fb8c4
Jasin <jasin@coleburt.com>
2014-05-02 13:22:35 -0400

    fixed indentation

--- proxy-server.js
+++ proxy-server.js
@@ -1,7 +1,7 @@
 // include required js
 var http = require('http'),  
-    httpProxy = require('http-proxy'),
-    fs = require('fs');
+	httpProxy = require('http-proxy'),
+	fs = require('fs');
 
 // read the config file and set some variables
 var obj = JSON.parse(fs.readFileSync('./config', 'utf8')),
@@ -11,25 +11,25 @@ var obj = JSON.parse(fs.readFileSync('./config', 'utf8')),
 // setup our proxy server to route request
 var proxy = httpProxy.createProxyServer();
 var server = http.createServer(function(req, res){
-    if(subdomain[req.headers.host]) {
-	console.log('Routing a request for ' + req.headers.host);
-        proxy.web(req, res, {
-            target: 'http://localhost:' + subdomain[req.headers.host]
-        }, function(e) {
-            console.log('Error: ' + e['code']);
-        });
-    } else {
-	console.log('Routing default request for unknown subdomain ' + req.headers.host);
-        proxy.web(req, res, {
-            target: 'http://localhost:9000'
-        });
-    }
+	if(subdomain[req.headers.host]) {
+		console.log('Routing a request for ' + req.headers.host);
+		proxy.web(req, res, {
+			target: 'http://localhost:' + subdomain[req.headers.host]
+		}, function(e) {
+			console.log('Error: ' + e['code']);
+		});
+	} else {
+		console.log('Routing default request for unknown subdomain ' + req.headers.host);
+		proxy.web(req, res, {
+			target: 'http://localhost:9000'
+		});
+	}
 }).listen(port);
 
 // default server to route incase there is no matching subdomain
 var base = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('<h3>Oops! We had an issue, blame Rob!</h3>');
+	res.writeHead(200, {'Content-type': 'text/html'});
+	res.end('<h3>Oops! We had an issue, blame Rob!</h3>');
 }).listen(9000);
 
 console.log('Proxy server listening on port ' + port);
