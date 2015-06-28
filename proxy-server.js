5dea002f5381206c0a8eb6279045d9671720be1f
Jasin <jasin@coleburt.com>
2015-06-28 14:02:54 -0400

    Refactored to reduce code size and make it easier to update

--- proxy-server.js
+++ proxy-server.js
@@ -1,4 +1,4 @@
-// include required js
+
 var http = require('http'),  
 	httpProxy = require('http-proxy'),
 	fs = require('fs'),
@@ -11,28 +11,74 @@ var obj = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'config'), 'utf8'))
 	subdomain = obj.subdomain,
 	error_file = obj.error_file;
 
-// setup our proxy server to route request
-var proxy = httpProxy.createProxyServer();
-var server = http.createServer(function(req, res){
-	if(subdomain[req.headers.host]) {
-		console.log('Routing a request for ' + req.headers.host);
-		proxy.web(req, res, {
-			target: 'http://localhost:' + subdomain[req.headers.host]
-		}, function(e) {
-			console.log('Error: ' + e.code);
-		});
-	} else {
-		console.log('Routing default request for unknown subdomain ' + req.headers.host);
-		proxy.web(req, res, {
-			target: 'http://localhost:' + web_port
-		}, function(e) {
-			console.log('Error: ' + e['code']);
-		});
-	}
-}).listen(proxy_port);
+// declare our variables
+var proxy;
+var serverip4;
+var serverip6;
 
-// default server to route incase there is no matching subdomain
-var base = http.createServer(function(req, res){
+// The server that will be routing the actual request
+proxy = httpProxy.createProxyServer({ ws: true });
+
+// Setup to listen for web request on IPv4/6
+serverip4 = http.createServer( function(req, res){
+	webRoute(req, res);	
+});
+
+serverip6 = http.createServer( function(req, res){
+	webRoute(req, res);
+});
+
+// Setup to listen for websockets on IPv4/6
+serverip4.on('upgrade', function(req, res) {
+	wsRoute(req, res);
+});
+	
+serverip6.on('upgrade', function(req, res) {
+	wsRoute(req, res);
+});
+	
+
+// initialize listening
+serverip4.listen(proxy_port, '127.0.0.1');
+serverip6.listen(proxy_port, '::1');
+
+// web route
+function webRoute(req, res) {
+	if(subdomain[req.headers.host]) {
+			console.log('Routing web request: ' + req.headers.host + ' From: ' + req.connection.remoteAddress);
+			proxy.web(req, res, {
+				target: 'http://localhost:' + subdomain[req.headers.host]
+			}, function(e) {
+				console.log('Error: ' + e.code);
+			});
+		} else {
+			defaultRoute();
+		}
+};
+
+// websocket route
+function wsRoute(req, res) {
+	console.log('Routing websocket request: ' + req.headers.host + ' From: ' + req.connection.remoteAddress);
+	proxy.ws(req, res , {
+		target: 'http://localhost:' + subdomain[req.headers.host]
+	}, function (e) {
+		console.log('Error: ' + e.code);
+	})
+};
+
+// default router
+function defaultRoute() {
+	console.log('Routing default request: ' + req.headers.host + ' From: ' + req.connection.remoteAddress);
+	proxy.web(req, res, {
+		target: 'http://localhost:' + web_port
+	}, function(e) {
+		console.log('Error: ' + e.code);
+	});
+};
+
+
+// default server to notify user subdomain is not valid
+var defaultServer = http.createServer(function(req, res){
 	fs.readFile(error_file, function(err, data) {
         if (err) {
             console.log(err);
